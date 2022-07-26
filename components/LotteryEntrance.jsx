71ba0caea63991c6e3f43c86a2d3b012df35ffdb
jsmjsm <jsmjsm.eth@gmail.com>
2022-07-26 23:56:56 +0800

    css

--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -6,17 +6,30 @@ import { ethers } from "ethers"
 import { useNotification } from "web3uikit"
 
 export default function LotteryEntrance() {
-    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis()
     const chainId = parseInt(chainIdHex)
     const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
-    // console.log(`chainId: ${chainId}`)
-    // console.log(`raffleAddress: ${raffleAddress}`)
+
+    // State hooks
     const [entranceFee, setEntranceFee] = useState("0")
     const [numPlayers, setNumPlayers] = useState("0")
     const [recentWinner, setRecentWinner] = useState("0")
 
     const dispatch = useNotification()
 
+    const {
+        data: enterTxResponse,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "enterRaffle",
+        msgValue: entranceFee,
+        params: {},
+    })
+
+    /* View Functions */
     const { runContractFunction: enterRaffle } = useWeb3Contract({
         abi: abi,
         contractAddress: raffleAddress,
@@ -46,7 +59,7 @@ export default function LotteryEntrance() {
         params: {},
     })
 
-    async function updateUI() {
+    async function updateUIValues() {
         const entranceFeeFromCall = (await getEntranceFee()).toString()
         const numPlayersFromCall = (await getNumPlayers()).toString()
         const recentWinner = (await getRecentWinner()).toString()
@@ -58,16 +71,9 @@ export default function LotteryEntrance() {
     // > Use Effect
     useEffect(() => {
         if (isWeb3Enabled) {
-            // try to read the raffle entrance fee
-            updateUI()
+            updateUIValues()
         }
     }, [isWeb3Enabled])
-
-    const handleSuccess = async function (tx) {
-        await tx.wait(1)
-        handleNewNotification(tx)
-        updateUI()
-    }
 
     const handleNewNotification = function () {
         dispatch({
@@ -79,27 +85,41 @@ export default function LotteryEntrance() {
         })
     }
 
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+        updateUIValues()
+    }
+
     return (
-        <div>
-            Hi from lottery entrance!{" "}
+        <div className="p-5">
+            <h1 className="py-4 px-4 font-bold text-3xl">Lottery</h1>
             {raffleAddress ? (
-                <div>
+                <>
                     <button
-                        onClick={async function () {
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+                        onClick={async () =>
                             await enterRaffle({
+                                // onComplete:
+                                // onError:
                                 onSuccess: handleSuccess,
                                 onError: (error) => console.log(error),
                             })
-                        }}
+                        }
+                        disabled={isLoading || isFetching}
                     >
-                        Enter Raffle
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                        ) : (
+                            "Enter Raffle"
+                        )}
                     </button>
-                    Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH Number of
-                    Players: {numPlayers}
-                    Recent Winner: {recentWinner}
-                </div>
+                    <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
+                    <div>The current number of players is: {numPlayers}</div>
+                    <div>The most previous winner was: {recentWinner}</div>
+                </>
             ) : (
-                <div> No Raffle Address Detected </div>
+                <div>Please connect to a supported chain </div>
             )}
         </div>
     )
